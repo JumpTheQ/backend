dda26d0c1e0cc9498e8b59502c2c282062f5cb5b
= <=>
2024-04-20 16:38:35 +0100

    Fix typo

--- config/services.php
+++ config/services.php
@@ -31,7 +31,7 @@ return [
         ],
     ],
 
-    'linked' => [
+    'linkedin' => [
         'client_id' => env('LINKEDIN_CLIENT_ID'),
         'client_secret' => env('LINKEDIN_CLIENT_SECRET'),
         'redirect' => env('FRONTEND_URL') . "/login?callback?platform=linkedin",
