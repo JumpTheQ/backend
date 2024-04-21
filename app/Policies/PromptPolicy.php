dd97e047a60409fbb7e25296a4a1c68142057ebe
= <=>
2024-04-21 12:47:58 +0100

    Fix policy again

--- app/Policies/PromptPolicy.php
+++ app/Policies/PromptPolicy.php
@@ -18,6 +18,14 @@ class PromptPolicy
     }
 
     /**
+     * Determine whether the user can create the model.
+     */
+    public function create(User $user, Application $application): bool
+    {
+        return $user->id === $application->user_id;
+    }
+
+    /**
      * Determine whether the user can view the model.
      */
     public function view(User $user, Prompt $prompt): bool
